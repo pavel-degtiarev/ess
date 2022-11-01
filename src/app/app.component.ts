9388ff29eb072dc67489cb5afa2edd3df0d91162
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-11-01 19:52:38 +0700

    в режиме формы ссылка на изменение API Key блокируется

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 import { APIKEY_URL, HOME_URL } from './modules/routing/routing.module';
+import { LockService } from './services/lock.service';
 
 interface ILink {
   title: string;
@@ -23,13 +24,13 @@ export class AppComponent implements OnInit {
 
   link: ILink;
 
-  constructor(private router: Router) {
-    this.link = links.home;
-  }
+  constructor(private router: Router, public lockService: LockService) {}
 
   // Подписываемся на событие навигации, смотрим, на каком url находимся
   // и меняем надпись и routerLink у ссылки в template.
   ngOnInit(): void {
+    this.link = links.home;
+    
     this.router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
