df2169eab597729ab3fbc60cd5b2e5111e81e836
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-11-01 18:02:01 +0700

    динамически меняем состояние ссылки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { APIKEY, HOME } from './modules/routing/routing.module';
 
 interface ILink {
@@ -19,14 +21,32 @@ const links = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ess';
 
   link: ILink;
 
-  constructor() {
+  constructor(private router: Router) {
     this.link = links.home;
   }
 
-
+  // Подписываемся на событие навигации, смотрим, на каком url находимся
+  // и меняем надпись и routerLink у ссылки в template.
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map((event: any) => event.url)
+      )
+      .subscribe((url) => {
+        switch (url) {
+          case HOME_URL:
+            this.link = links.apikey;
+            break;
+          case APIKEY_URL:
+            this.link = links.home;
+            break;
+        }
+      });
+  }
 }
