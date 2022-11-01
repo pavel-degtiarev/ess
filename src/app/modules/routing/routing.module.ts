aff5a3e5783520b2e3bf7837e44207f0ca1fd634
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-11-01 20:23:22 +0700

    добавлен Гард, не дающий перейти на изменение API Key из режима формы

--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListComponent } from 'src/app//components/users-list/users-list.component';
 import { ApiKeyInputComponent } from 'src/app/components/api-key-input/api-key-input.component';
+import { LockGuard } from 'src/app/guards/lock.guard';
 
-const HOME = "";
+const HOME = '';
 const APIKEY = 'api-key';
 
 export const HOME_URL = `/${HOME}`;
@@ -12,12 +13,13 @@ export const APIKEY_URL = `/${APIKEY}`;
 
 const routes: Routes = [
   { path: HOME, component: UsersListComponent },
-  { path: APIKEY, component: ApiKeyInputComponent },
+  { path: APIKEY, component: ApiKeyInputComponent, canActivate: [LockGuard] },
 ];
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, RouterModule.forRoot(routes)],
+  providers: [LockGuard],
   exports: [RouterModule],
 })
 export class RoutingModule {}
